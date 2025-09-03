91d90c45c6beab397f0595485477299843f8e3ae
claudeteoliveira <silveiraclaudete651@gmail.com>
2025-09-02 21:51:59 -0300

    criacao do progra de tarefas

--- tarefas.py
+++ tarefas.py
@@ -1,5 +1,13 @@
-numero_1 = int(input("informe o primeiro numero"))
-print(("o dobro é -> ", numero_1 * 2))
-lado_1 = int(input("informe o primeiro lado"))
-print("lado * lado é ->", lado_1 * lado_1 )
-idade1SS
+#calculando um numero informado
+#numero_1 = int(input("informe o primeiro numero"))
+#print(("o dobro é -> ", numero_1 * 2))
+#lado = float(input("informe o lado do quadrado ->"))
+#print("area do quadrado é ->", lado * lado )
+idade = int(input("diga sua idade -> "))
+meses = int(idade* 12)
+dias = meses * 365
+print("sua idade em meses é", meses, "e em dias", dias)
+nota1 = flout(input("informe a primeira nota ->"))
+nota2 =flout6(input("informe a segunda nota ->"))
+nota3 = flout(input(" informe a terceira nota ->"))
+print("a média ´e -> ->", (nota1 + nota2 + nota3))
