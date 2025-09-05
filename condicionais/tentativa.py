1ce1d4159caf1cf9cfc7b164351c5134a6834e97
claudeteoliveira <silveiraclaudete651@gmail.com>
2025-09-05 12:08:56 -0300

    T 1

---
+++ condicionais/tentativa.py
@@ -0,0 +1,13 @@
+# print ('bom dia')
+# nome = input('diga seu nome ? ')
+# print("ola",nome)
+# idade = input('qual é sua idade ? ') 
+
+# endereço = input('onde mora ? ')
+# idade = int(input('diga a sua idade:'))
+# print('ano que vem você terá',id
+
+numero_1 = int(input('informe o primeiro numero'))
+print("o dobro é ->",numero_1 * 2)
+lado_1 = int(input('informe o primeiro lado'))
+print('lado + lado é ->',lado_1 + lado_1 )
