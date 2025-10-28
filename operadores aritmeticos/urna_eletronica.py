8865165b7a8ab3656b9d9aee9de56bfe7f246be2
claudeteoliveira <silveiraclaudete651@gmail.com>
2025-10-27 21:56:50 -0300

    vd

---
+++ operadores aritmeticos/urna_eletronica.py
@@ -0,0 +1,31 @@
+candidatos = {'josé': '11',
+              'joão': '22',
+              'pedro': '33'}
+
+for chave,  numero in candidatos.items():
+    print(f'numero {numero} | ', end=' ')
+
+    print('\n')
+
+for chave, numero in candidatos.values():
+    print(f'chave{chave} | ', end=' ')
+
+    print('\n')
+
+for chave, voto in candidatos.items():
+    print(f'chave {chave} | voto {voto}')
+
+    print('\n')
+
+
+if num_votos == ('josé': '11'):
+     print('200 VOTOS')
+elif opção == 2:
+    soma_votos = 0
+
+for candidato in candidato.values():
+        soma_votos += candidatos['votos']
+
+print(f'a média dos votos é {soma_notas / len(candidatos)}')
+
+
