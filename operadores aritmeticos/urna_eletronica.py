d25e7564e68e5a764280a342890f22ac3cee594b
claudeteoliveira <silveiraclaudete651@gmail.com>
2025-10-27 21:57:22 -0300

    urna

--- operadores aritmeticos/urna_eletronica.py
+++ operadores aritmeticos/urna_eletronica.py
@@ -1,31 +1,92 @@
-candidatos = {'josé': '11',
-              'joão': '22',
-              'pedro': '33'}
+opção = -1
+senha_encerrar = 1903
+encerrar_votaçao=1900
+candidatos = []
+contagem_votos ={'nulos': 0}
 
-for chave,  numero in candidatos.items():
-    print(f'numero {numero} | ', end=' ')
+#menu principal
+while opção != senha_encerrar:
+    opção = input('o que deseja fazer? \n'
+                  'opção 1: cadastrar candidato \n'
+                  'opção 2:iniciar votação'
+                  'opção 3: mostrar voto e vencedor \n'
+                  'opção 0: sair \n\n'
+                  'opção -> ')
+    
+    #valida se a opção é um numero
+    if opção.isdigit():
+        opção = int(opção)
 
-    print('\n')
+        #testar para opção urna
+        if opção == 1:
+            qtd_candidatos = int(input('qtd candidatos deseja cadastrar -> '))
+            
+            for c in range(1, qtd_candidatos+1):
+                candidato = []
+                nome_candidato = input(f'nome do candidato {c} -> ')
+                num_candidato = int(input(f'numero do candidato {c} -> '))
 
-for chave, numero in candidatos.values():
-    print(f'chave{chave} | ', end=' ')
+                #salva candidato
+                candidato.append(nome_candidato)
+                candidato.append(num_candidato)
+                #adiciona candidato na lista principal
+                candidato.append(tuple(candidato))
 
-    print('\n')
+            print('\n\n')
 
-for chave, voto in candidatos.items():
-    print(f'chave {chave} | voto {voto}')
+        if opção == 2:
+            voto =-1
+        
+            #mostrar lista de candidatos
+            while voto != encerrar_votaçao:
+                for candidato in candidatos:
+                    print(f'candidato {candidato[0]} | numero{candidato[1]}')
+                          
+                voto = int(input('vote no numero de um candidato ->'))
 
-    print('\n')
+                #contabilizar a votação
+                cont = 0
+                for candiudato in candidatos:
+                    cont += 1
+                    if voto == candidato[1]:
+                        if candidato[0] not in contagem_votos:
+                            contagem_votos.update({candidato[0]: 1})
+                            break
+                        else:
+                            contagem_votos[candidato[0]] += 1
+                            break
+                    else:
+                        if cont == len(candidatos):
+                            contagem_votos['nulos'] += 1
+        elif opção == 3:
+            #remove nulo incorreto da saida da votação '1900'
+            contagem_votos['nulos'] -= 1
+
+            #mostra resultados e vencedor
+            maioir = 0
+            vencedor = ''
+            for key, value in contagem_votos.items():
+                if value > maior:
+                    maior = value
+                    vencedor = key
+                print(f'{key} | votos {value}')
+
+            print(f'o vencedor é {vencedor} com {maior} votos')
+            print('\n\n')
+        # se a opção digitada não for um numero cai aqui
+    else:
+        print('\n\nopção invalida\n\n')                    
+                
 
 
-if num_votos == ('josé': '11'):
-     print('200 VOTOS')
-elif opção == 2:
-    soma_votos = 0
 
-for candidato in candidato.values():
-        soma_votos += candidatos['votos']
 
-print(f'a média dos votos é {soma_notas / len(candidatos)}')
+
+
+
+
+
+
+#valida se a 0pção
 
 
